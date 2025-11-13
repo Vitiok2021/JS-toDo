b3a406873edad134b6b270cdb1faa51173a17028
Vitiok2009 <viktorpts1vol@gmail.com>
2025-11-13 08:04:39 +0200

    Added button to delete

--- js/scripts.js
+++ js/scripts.js
@@ -8,20 +8,30 @@ let tasks = [
 function render() {
   let list = document.getElementsByClassName('task-list')
   list[0].innerHTML = ''
-  let item
+
   for (let i = 0; i < tasks.length; i++) {
-    item = document.createElement('li')
+    let item = document.createElement('li')
     item.classList.add('task-item')
+    let btn = document.createElement('button')
+    btn.addEventListener('click', () => delTask(i))
+    btn.classList.add('del-btn')
+    btn.innerHTML = 'Видалити'
     item.textContent = tasks[i].title
+    item.append(btn)
     list[0].append(item)
   }
 }
 document.querySelector('.add-task').addEventListener('click', addTask)
+
 function addTask() {
   let val = document.querySelector('.new-task').value
-  console.log(val)
+  //   console.log(val)
   tasks.push({ title: val, checked: false })
   render()
 }
 function toggleTask() {}
+function delTask(index) {
+  tasks.splice(index, 1)
+  render()
+}
 render()
