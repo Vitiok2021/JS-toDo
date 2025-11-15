68a3a12bb3b2e9023b6ec7149b2dcbb242c84dfc
Vitiok2009 <viktorpts1vol@gmail.com>
2025-11-15 11:53:34 +0200

    added filtration

--- js/scripts.js
+++ js/scripts.js
@@ -15,31 +15,35 @@ function render() {
   let list = document.getElementsByClassName('task-list')
   list[0].innerHTML = ''
 
-  let filteredTasks = [...tasks]
-
-  if (filteredTasks === activeAll) {
+  let filteredTasks = tasks.map((t, index) => ({ ...t, index }))
+  if (activeFilter === newTask) {
+    filteredTasks = filteredTasks.filter((t) => !t.checked)
+  }
+  if (activeFilter === chked) {
+    filteredTasks = filteredTasks.filter((t) => t.checked)
   }
 
   let counter = tasks.length
   let done = tasks.filter((t) => t.checked).length
-  document.querySelector('.counter-item').innerHTML = counter
+  document.querySelector('.counter-item').innerHTML = `${counter}. Виконані: ${done}`
 
-  for (let i = 0; i < tasks.length; i++) {
+  for (let i = 0; i < filteredTasks.length; i++) {
     let item = document.createElement('li')
     item.classList.add('task-item')
     let btn = document.createElement('button')
-    btn.addEventListener('click', () => delTask(i))
+    btn.addEventListener('click', () => delTask(filteredTasks[i].index))
     btn.classList.add('del-btn')
     btn.innerHTML = 'Видалити'
     let chk = document.createElement('input')
     chk.type = 'checkbox'
-    chk.checked = tasks[i].checked
+    chk.checked = filteredTasks[i].checked
     chk.addEventListener('change', () => {
-      tasks[i].checked = chk.checked
+      let realIndex = filteredTasks[i].index
+      tasks[realIndex].checked = chk.checked
       render()
     })
     item.append(chk)
-    item.append(document.createTextNode(tasks[i].title))
+    item.append(document.createTextNode(filteredTasks[i].title))
     item.append(btn)
     list[0].append(item)
   }
@@ -53,9 +57,20 @@ function addTask() {
   render()
 }
 
-function toggleTask() {}
 function delTask(index) {
   tasks.splice(index, 1)
   render()
 }
+document.querySelector('.show-all').addEventListener('click', () => {
+  activeFilter = activeAll
+  render()
+})
+document.querySelector('.ended').addEventListener('click', () => {
+  activeFilter = chked
+  render()
+})
+document.querySelector('.new').addEventListener('click', () => {
+  activeFilter = newTask
+  render()
+})
 render()
